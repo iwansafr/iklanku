f5f77931373af48ec86c9cccf75d4ecf574f6435
iwansafr <iwansafr@gmail.com>
2020-05-30 04:57:36 +0700

    bugfix path css magnific

--- application/modules/home/views/templates/iklanku/meta.php
+++ application/modules/home/views/templates/iklanku/meta.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="<?php echo base_url('templates/iklanku/css/');?>magnific-popup/magnific-popup.css">
+<link rel="stylesheet" href="<?php echo base_url('templates/iklanku/css/');?>magnific-popup.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('templates/iklanku/css/style.css');?>" />
