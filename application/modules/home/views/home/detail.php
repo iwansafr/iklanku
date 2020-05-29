ecd793f1ca350415e02d0c7d872c73c364f7857a
iwansafr <iwansafr@gmail.com>
2020-05-30 04:22:01 +0700

    textsize detail

--- application/modules/home/views/home/detail.php
+++ application/modules/home/views/home/detail.php
@@ -1,4 +1,9 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<style type="text/css">
+	body{
+		background-color: white!important; 
+	}
+</style>
 <div class="divider"></div>
 <div class="content_top">
 	<nav aria-label="breadcrumb">
@@ -9,16 +14,15 @@
 	</nav>
 </div>
 <center>
-	<span class="badge badge-success pull-right mt-1" style="width: 20vw; position: absolute; left: 10px;  font-size: 3vw;">available</span>
 	<img src="<?php echo base_url('templates/iklanku');?>/img/map.jpg" class="border img img-fluid m-auto" alt="">
 </center>
 <div class="review container-fluid">
 	<div class="row">
 		<div class="col">
 			<div class="views mb-2">
-				<small class="text-primary" style="font-size: 3vw;"><i class="fa fa-eye"></i> 360 views</small>
+				<small class="text-primary"><i class="fa fa-eye"></i> 360 views</small>
 			</div>
-			<table class="table table-sm" style="font-size: 3vw;"> 
+			<table class="table table-sm"> 
 				<tr>
 					<td>
 						<small>ukuran</small>			
@@ -50,16 +54,12 @@
 		</div>
 		<div class="col">
 			<div class="rating text-right mb-3" style="font-size: 3vw;">
-				<span class="rating_star fa fa-star checked"></span>
-				<span class="rating_star fa fa-star checked"></span>
-				<span class="rating_star fa fa-star checked"></span>
-				<span class="rating_star fa fa-star checked"></span>
-				<span class="rating_star fa fa-star"></span>
+				<span class="badge badge-success pull-right mt-1" style="width: 20vw; font-size: 3vw;">available</span>
 			</div>
 			<div class="description pl-3" style="min-height: 100px;">
 				<i class="fa fa-map-marker"style="position: absolute;top: 40%;left: 0;font-size: 5vw;"></i>
-				<span style="font-size: 3vw;">< 50 Meter</span>
-				<p class="text-muted" style="font-size: 2vw;">
+				<span>< 50 Meter</span>
+				<p class="text-muted">
 					RM. Pempek Ny. Kamto BRI Unit Majapahit SPBU Majapahit
 				</p>
 			</div>
