e219dc618e443387cf0ee03c66371e7398c6c598
iwansafr <iwansafr@gmail.com>
2020-07-22 07:33:24 +0700

    update module iklan

--- application/modules/admin/views/iklan/edit.php
+++ application/modules/admin/views/iklan/edit.php
@@ -6,4 +6,19 @@ $form->init('edit');
 $form->setTable('iklan');
 
 $form->addInput('jalan','text');
+$form->addInput('kota','text');
+$form->addInput('koordinat','hidden');
+$form->addInput('map_image','file');
+$form->setLabel('map_image','Gambar Map');
+$form->setAccept('map_image','.jpg,.png,.gif');
+
+$form->addInput('gallery','files');
+$form->addInput('ukuran','text');
+$form->setType('ukuran','number');
+
+$form->addInput('dimensi','dropdown');
+$form->setOptions('dimensi',['1'=>'Horizontal','2'=>'Vertical']);
+
+
+
 $form->form();
