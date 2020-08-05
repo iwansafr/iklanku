8db2e0613352d68635f2401fe9d1d092bef00eb8
iwansafr <iwansafr@gmail.com>
2020-08-05 11:21:55 +0700

    desain mode

--- application/modules/home/views/iklan/media.php
+++ application/modules/home/views/iklan/media.php
@@ -1,13 +1,13 @@
-<!-- <div class="container"> -->
-	<div class="row mt-5 mt-5 pt-5">
+<div class="container mt-5 mt-5 pt-5">
+	<div class="row">
 		<div class="col">
 			<a href="<?php echo base_url('home/iklan') ?>">
 				<div class="card card-success" style="height: 300px;">
 					<div class="card-body">
-						<h3 style="margin-top: 30%;text-align: center;">OUT OF HOME MEDIA</h3>
+						<h4 style="margin-top: 30%;text-align: center;">OUT OF HOME MEDIA</h4>
 					</div>
-					<div class="card-footer">
-						<p style="font-size: 12px;">Billboard, Baliho, Neon Box Banner, JPO, Videotron, TV Wll</p>
+					<div class="card-footer" style="height: 100px;">
+						<p style="font-size: 10px;">Billboard, Baliho, Neon Box Banner, JPO, Videotron, TV Wall</p>
 					</div>
 				</div>
 			</a>
@@ -15,10 +15,10 @@
 		<div class="col">
 			<div class="card card-success" style="height: 300px;">
 				<div class="card-body">
-					<h3 style="margin-top: 30%;text-align: center;">RADIO</h3>
+					<h4 style="margin-top: 30%;text-align: center;">RADIO</h4>
 				</div>
-				<div class="card-footer">
-					<p style="font-size: 12px;">Radio Mainstream di Seluruh Jawa Tengah</p>
+				<div class="card-footer" style="height: 100px;">
+					<p style="font-size: 10px;">Radio Mainstream di Seluruh Jawa Tengah</p>
 				</div>
 			</div>
 		</div>
@@ -28,22 +28,22 @@
 		<div class="col">
 			<div class="card card-success" style="height: 300px;">
 				<div class="card-body">
-					<h3 style="margin-top: 30%;text-align: center;">KORAN</h3>
+					<h4 style="margin-top: 30%;text-align: center;">KORAN</h4>
 				</div>
-				<div class="card-footer">
-					<p style="font-size: 12px;">Koran Lokal maupun Nasional</p>
+				<div class="card-footer" style="height: 100px;">
+					<p style="font-size: 10px;">Koran Lokal maupun Nasional</p>
 				</div>
 			</div>
 		</div>
 		<div class="col">
 			<div class="card card-success" style="height: 300px;">
 				<div class="card-body">
-					<h3 style="margin-top: 30%;text-align: center;">MEIDA SOCIAL CAMPAIGN</h3>
+					<h4 style="margin-top: 30%;text-align: center;">MEIDA SOCIAL CAMPAIGN</h4>
 				</div>
-				<div class="card-footer">
-					<p style="font-size: 12px;">Instagram, Facebook Twitter, etc</p>
+				<div class="card-footer" style="height: 100px;">
+					<p style="font-size: 10px;">Instagram, Facebook Twitter, etc</p>
 				</div>
 			</div>
 		</div>
 	</div>	
-<!-- </div> -->
+</div>
