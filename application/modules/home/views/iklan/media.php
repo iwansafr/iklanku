b212476378852007f56ee62a25ea95f3d940b860
iwansafr <iwansafr@gmail.com>
2020-08-11 00:35:10 +0700

    underconstruction

--- application/modules/home/views/iklan/media.php
+++ application/modules/home/views/iklan/media.php
@@ -11,25 +11,43 @@
 		<div class="col text-center">
 			<a href="<?php echo base_url('home/iklan') ?>">
 				<img src="<?php echo base_url('images/ooh_active.png') ?>" class="img img-fluid img-circle">
-				<p style="text-align: center;">OUT OF HOME MEDIA</p>
+				<p style="text-align: center;">OUT OF HOME<br>MEDIA</p>
 			</a>
 		</div>
 		<div class="col text-center">
-			<a href="<?php echo base_url('home/iklan') ?>">
+			<a href="#" data-toggle="modal" data-target="#sosmedModal">
 				<img src="<?php echo base_url('images/sosmed_disabled.png') ?>" class="img img-fluid img-circle">
 				<p class="disabled" style="text-align: center;">SOCIAL MEDIA CAMPAIGN</p>
 			</a>
+			<div class="modal fade" id="sosmedModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+			  <div class="modal-dialog modal-dialog-centered" role="document">
+			    <div class="modal-content">
+			      <div class="modal-header">
+			        <h5 class="modal-title" id="exampleModalLongTitle"></h5>
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			          <span aria-hidden="true">&times;</span>
+			        </button>
+			      </div>
+			      <div class="modal-body">
+			      	<h5>UNDERCONSTRUCTION</h5>
+			      </div>
+			      <div class="modal-footer">
+			        
+			      </div>
+			    </div>
+			  </div>
+			</div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col text-center">
-			<a href="<?php echo base_url('home/iklan') ?>">
+			<a href="#" data-toggle="modal" data-target="#sosmedModal">
 				<img src="<?php echo base_url('images/radio_disabled.png') ?>" class="img img-fluid img-circle">
 				<p class="disabled" style="text-align: center;">IKLAN RADIO</p>
 			</a>
 		</div>
 		<div class="col text-center">
-			<a href="<?php echo base_url('home/iklan') ?>">
+			<a href="#" data-toggle="modal" data-target="#sosmedModal">
 				<img src="<?php echo base_url('images/koran_disabled.png') ?>" class="img img-fluid img-circle">
 				<p class="disabled" style="text-align: center;">IKLAN KORAN</p>
 			</a>
@@ -37,7 +55,7 @@
 	</div>
 	<div class="row">
 		<div class="col text-center">
-			<a href="<?php echo base_url('home/iklan') ?>">
+			<a href="#" data-toggle="modal" data-target="#sosmedModal">
 				<img src="<?php echo base_url('images/digitalprint_disabled.png') ?>" class="img img-fluid img-circle">
 				<p class="disabled" style="text-align: center;">DIGITAL PRINT</p>
 			</a>
