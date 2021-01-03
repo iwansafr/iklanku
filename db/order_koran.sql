07585fb5ed020a696bd783456e93436931f89c3e
iwansafr <iwansafr@gmail.com>
2021-01-04 05:22:14 +0700

    db order koran

---
+++ db/order_koran.sql
@@ -0,0 +1,42 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+DROP TABLE IF EXISTS `order_koran`;
+CREATE TABLE `order_koran` (
+  `id` int NOT NULL,
+  `media_id` int NOT NULL,
+  `nama_media` varchar(255) NOT NULL,
+  `alamat_media` varchar(255) NOT NULL,
+  `biaya` int NOT NULL,
+  `kolom` int NOT NULL,
+  `baris` int NOT NULL,
+  `tipe` tinyint(1) NOT NULL COMMENT '1=text, 2 = graphic',
+  `isi` text NOT NULL,
+  `user_id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `hp` varchar(255) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `order_koran`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `order_koran`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
